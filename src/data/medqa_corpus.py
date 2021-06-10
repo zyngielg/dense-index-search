7ca3071a821b46edc3012c662db4c81dfab0cd75
zyngielg <gustaw.zyngiel@gmail.com>
2021-06-10 10:34:12 +0200

    Add filtering of medqa textbooks

--- src/data/medqa_corpus.py
+++ src/data/medqa_corpus.py
@@ -1,10 +1,19 @@
 import os
 from nltk.stem.snowball import SnowballStemmer
+from nltk import sent_tokenize, word_tokenize
+from tqdm import tqdm
+from itertools import filterfalse
+import time
 
 
 class MedQACorpus():
     stemmer = SnowballStemmer(language='english')
-    textbooks_data_dir = 'data/medqa/textbooks/'
+    textbooks_dir = 'data/medqa/textbooks/'
+    filtered_textbooks_dir = 'data/medqa/textbooks_filtered/'
+    filtered_textbooks_log = 'data/medqa/textbooks_filtering_log.txt'
+    short_sentence_counter = 0
+    long_word_sentences_counter = 0
+    num_words_removed = 0
 
     def __init__(self, stemming=False) -> None:
         self.stemming = stemming
@@ -16,11 +25,80 @@ class MedQACorpus():
             return ' '.join(tokens)
         corpus = {}
 
-        for textbook_name in os.listdir(self.textbooks_data_dir):
-            textbook_path = self.textbooks_data_dir + '/' + textbook_name
+        if not os.path.exists(self.filtered_textbooks_dir):
+            self.filter_textbooks()
+
+        for book_title_txt in os.listdir(self.filtered_textbooks_dir):
+            textbook_path = self.filtered_textbooks_dir + '/' + book_title_txt
             with open(textbook_path, 'r') as textbook_file:
                 textbook_content = textbook_file.read()
                 if self.stemming:
                     textbook_content = stem_content(textbook_content)
-                corpus[textbook_name] = textbook_content
+                corpus[book_title_txt[:-4]] = textbook_content
         return corpus
+
+    def filter_textbooks(self):
+        os.mkdir(self.filtered_textbooks_dir)
+
+        for book_title_txt in os.listdir(self.textbooks_dir):
+            textbook_path = self.textbooks_dir + '/' + book_title_txt
+            with open(textbook_path, 'r') as textbook_file:
+                content = textbook_file.read()
+
+                # remove new lines and tabs
+                content = content.replace('\n', ' ').replace(
+                    '\r', ' ').replace('\t', ' ').strip()
+
+                # split to sentences
+                sentences = sent_tokenize(content)
+                num_sentences_before = len(sentences)
+                num_words_before = sum([len(word_tokenize(x)) for x in sentences])
+                # remove
+                sentences = list(filterfalse(self.__filter_short, sentences))
+                sentences = list(filterfalse(
+                    self.__filter_with_long_words, sentences))
+                num_removed = self.short_sentence_counter + self.long_word_sentences_counter
+                num_sentences_after = len(sentences)
+                num_words_after = sum([len(word_tokenize(x)) for x in sentences])
+                assert num_sentences_before-num_sentences_after == num_removed
+                assert num_words_before-num_words_after == self.num_words_removed
+                
+                self.__log_message(book_title_txt[:-4], num_sentences_before, num_sentences_after, num_words_before, num_words_after)
+                
+                self.short_sentence_counter = 0
+                self.long_word_sentences_counter = 0
+                self.num_words_removed = 0
+
+                content_filtered = ' '.join(sentences)
+                filtered_textbook_path = f"{self.filtered_textbooks_dir}/{book_title_txt}"
+                with open(filtered_textbook_path, 'w') as filtered_textbook_file:
+                    filtered_textbook_file.write(content_filtered)
+
+    def __filter_short(self, sentence):
+        tokens = word_tokenize(sentence)        
+
+        res = len(tokens) <= 5
+        if res:
+            self.short_sentence_counter += 1
+            self.num_words_removed += len(tokens)
+        return res
+
+    def __filter_with_long_words(self, sentence):
+        tokens = word_tokenize(sentence)
+        max_len = 35
+
+        lenghts = [len(x) for x in tokens]
+        if any(x > max_len for x in lenghts):
+            self.long_word_sentences_counter += 1
+            self.num_words_removed += len(tokens)
+            return True
+        return False
+    
+    def __log_message(self, title, num_sent_before, num_sent_after, num_words_before, num_words_after):
+        log_message = f"[{title}] \n\t#sentences: \tbefore: {num_sent_before}\tafter: {num_sent_after} -> removed: {num_sent_before-num_sent_after}\n"
+        log_message += f"\tRemoved {self.short_sentence_counter} sentences which had less than 6 words and after {self.long_word_sentences_counter} which contained a word of length over 35"
+        log_message += f"\t#words:     \tbefore: {num_words_before}\tafter: {num_words_after} -> removed: {num_words_before-num_words_after}\n"
+        print(log_message)
+        with open(self.filtered_textbooks_log, 'a') as log_file:
+            log_file.write(log_message)
+        
